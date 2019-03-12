801737a43825a12c09d06fafa13b6f291ea93d7f
= <=>
2019-03-12 22:21:44 +0200

    update 1.ps1
    Signed-off-by: = <=>

--- 1.ps1
+++ 1.ps1
@@ -1,24 +1,30 @@
-# TODO here check privileages
-if (-Not (.\Check_Administrator.ps1)) {
-(new-object -comobject wscript.shell).popup("Log out from this user and run this script from administrator account please",0,"Error message")
-}
-# To rename computer
-$computer_name = "DC01"
-Rename-Computer $computer_name
+## This script will change computer name , give it a static ip 
+## & run another scripts so be aware what you are doing ..
 
-# To give a static ip & dns
+## used variables
+$computer_name = "DC01"
+
 $IP = "10.10.40.1"
 $Mask_Number = "8"
 $DNS_Ip = $IP
-#1 $gate_way_ip
+#$gate_way_ip = "10.10.40.1"
+$Interface_Type = "Ethernet"
 
+## script start
+if (-Not (.\Check_Administrator.ps1)) {
+(new-object -comobject wscript.shell).popup("Log out from this user and run this script from administrator account please",0,"Error message")
+}
+
+# To rename computer
+Rename-Computer $computer_name
+
+# To give a static ip & dns
 New-NetIPAddress `
 -IPAddress $IP `
--InterfaceAlias "Ethernet" `
+-InterfaceAlias $Interface_Type `
 -AddressFamily IPv4 `
--PrefixLength $Mask_Number #1 `
-#1 -DefaultGateway $gate_way_ip
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $DNS_Ip
+-PrefixLength $Mask_Number #-DefaultGateway $gate_way_ip
+Set-DnsClientServerAddress -InterfaceAlias $Interface_Type -ServerAddresses $DNS_Ip
 
 # To restart computer
 Restart-Computer
