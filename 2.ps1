f692e13ba42705f4e3bf8c070ad72ff82aacc35b
khalil2535 <khalil2535@protonmail.com>
2019-03-13 16:18:56 +0200

    remove autorun after finish from
    Signed-off-by: khalil2535 <khalil2535@protonmail.com>

--- 2.ps1
+++ 2.ps1
@@ -6,12 +6,17 @@
 ## variables can be parameters
 $Domain_Name = "ucas.edu"
 $Netbios_Name = "UCAS"
-$Domain_Mode = "Win2008" # there is Win2012 too
-$Forest_Mode = "Win2008" # there is Win2012 too
+$Domain_Mode = "Win2008" # can be Win2012 too
+$Forest_Mode = "Win2008" # can be Win2012 too
 
 $Safe_Mode_Pass = ConvertTo-SecureString "Ucas!" -AsPlainText -Force
 
 ## script start
+
+# removing auto generated autorun (while running 2.ps1)
+if (Test-Path "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\autostart.cmd"){
+rm "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\autostart.cmd"
+}
 
 # installing Active Directory Domain Services
 Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
@@ -21,13 +26,14 @@ Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\Windows\NTDS" `
--DomainMode "Win2008" `
+-DomainMode $Domain_Mode `
 -DomainName $Domain_Name `
--DomainNetbiosName "UCAS" `
--ForestMode "Win2008" `
+-DomainNetbiosName $Netbios_Name `
+-ForestMode $Forest_Mode `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
 -NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true `
 -SafeModeAdministratorPassword $Safe_Mode_Pass
+
