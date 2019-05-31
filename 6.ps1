5258d5192c4cf5aa0d76260d39583a0667998740
mahmoud khalil <khalil2535@protonmail.com>
2019-05-31 16:22:48 +0300

    import saved gpo and add firefox admx
    Signed-off-by: mahmoud khalil <khalil2535@protonmail.com>

--- 6.ps1
+++ 6.ps1
@@ -1,5 +1,9 @@
 ## This script will configure the group policies.
 
+# import firefox admin template
+Move-Item (".\lib\Firefox\") ("C:\Windows\PolicyDefinitions") -Force
+Remove-Item (".\lib\Firefox")
+
 # commands for listting
 # Get-GPO -all
 # Get-GPO -name "Default Domain Policy"
@@ -9,7 +13,11 @@ new-gpo -name TechGPO `
 | set-gppermissions -replace -permissionlevel none -targetname "Authenticated Users" -targettype group `
 | set-gppermissions -permissionlevel gpoapply -targetname "Tech" -targettype group
 
+Import-GPO -BackupGpoName TechGPO -Path .\lib\GPO -TargetName TechGPO
+
 new-gpo -name ManagersGPO `
 | new-gplink -target "ou=Managers,dc=ucas,dc=edu" `
 | set-gppermissions -replace -permissionlevel none -targetname "Authenticated Users" -targettype group `
 | set-gppermissions -permissionlevel gpoapply -targetname "Managers" -targettype group
+
+Import-GPO -BackupGpoName ManagersGPO -Path .\lib\GPO -TargetName ManagersGPO
